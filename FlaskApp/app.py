cd32f65298a276f401b644ef8bccd0052ecf5552
japnitahuja <japnit.ahuja@gmail.com>
2022-09-17 13:04:17 -0400

    sample text for testing

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,34 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "../SpeechAPI/video"
 
 
-@app.route('/')
-def hello():
-    return '<form method = "post" action = "/video" ><input type = "file" id = "myFile" name = "filename" ><input type = "submit" ></ >'
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''

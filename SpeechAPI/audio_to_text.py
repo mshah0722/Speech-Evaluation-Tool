29b87f4b78215f0b3e2e6f7435f2390b1e0ef661
japnitahuja <japnit.ahuja@gmail.com>
2022-09-17 12:32:44 -0400

    video to text pipeline

--- SpeechAPI/audio_to_text.py
+++ SpeechAPI/audio_to_text.py
@@ -1,15 +1,37 @@
 from doctest import REPORT_CDIFF
 import requests
 import time
+from get_assembly_key import get_assembly_key
+
+# authorization
+headers = {
+    "authorization": get_assembly_key(),
+    "content-type": "application/json"
+}
+
+# upload local file to assembly
+filename = "audio/test.mp3"
+
+
+def read_file(filename, chunk_size=5242880):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(chunk_size)
+            if not data:
+                break
+            yield data
+
+
+response = requests.post('https://api.assemblyai.com/v2/upload',
+                         headers=headers,
+                         data=read_file(filename))
+
+audio_url = response.json()['upload_url']
 
 # post transcription request
 postEndpoint = "https://api.assemblyai.com/v2/transcript"
 json = {
-    "audio_url": "https://bit.ly/3yxKEIY"
-}
-headers = {
-    "authorization": "4c1fcafbf469465299912b663672b5c2",
-    "content-type": "application/json"
+    "audio_url": audio_url
 }
 response = requests.post(postEndpoint, json=json, headers=headers)
 

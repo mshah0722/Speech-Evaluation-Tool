7696688e7becb042da48efe72139c23af4fbb722
Dheeraj Vagavolu <dheeraj.vagavolu@uwaterloo.ca>
2022-09-18 03:12:00 -0400

    working

--- FlaskApp/video_to_audio.py
+++ FlaskApp/video_to_audio.py
@@ -1,11 +1,6 @@
 from moviepy.editor import *
 
-<<<<<<< HEAD
-def extractAudioFromMP4(mp4_path = "assets/test.mp4", wav_path = "audio/gen_audio.wav"):
-=======
-
 def extractAudioFromMP4(mp4_path, wav_path):
->>>>>>> ffe5624925465cd3388c0d2c3a45e62ba75bb3f7
 
     videoclip = VideoFileClip(mp4_path)
     audioclip = videoclip.audio
@@ -13,9 +8,3 @@ def extractAudioFromMP4(mp4_path, wav_path):
 
     audioclip.close()
     videoclip.close()
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> ffe5624925465cd3388c0d2c3a45e62ba75bb3f7
